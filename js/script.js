3ac17236e9bac983b5fd1240eeb3c05b871b95f2
Lerois <lerois.ll@gmail.com>
2024-08-01 23:08:54 +0300

    HW-2 completed

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//part1
+
+'use strict';
+
+let admin, name
+name = 'John'
+admin = name
+alert(admin)
+
+//part2
+
+let theEarth
+let username
+
+//part3
+
+let sum = '10' + 10
+const sumType = typeof sum
+console.log(sumType)
