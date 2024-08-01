4a71ab07fd172dbe4e22b1afcbabb0a527661fc8
Lerois <lerois.ll@gmail.com>
2024-08-01 23:14:27 +0300

    HW - 3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const userInput1 = prompt('Enter first variable')
+const userInput2 = prompt('Enter second variable')
+console.log(`Результат: ${userInput1}+${userInput2}=${parseInt(userInput1)+parseInt(userInput2)}`)
+console.log(`Результат: ${userInput1}-${userInput2}=${parseInt(userInput1)-parseInt(userInput2)}`)
+console.log(`Результат: ${userInput1}*${userInput2}=${parseInt(userInput1)*parseInt(userInput2)}`)
+console.log(`Результат: ${userInput1}/${userInput2}=${parseInt(userInput1)/parseInt(userInput2)}`)
