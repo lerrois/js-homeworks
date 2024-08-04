8945d81775d0a31d156790b27a937b3136f33ab9
Lerois <lerois.ll@gmail.com>
2024-08-04 22:35:45 +0300

    HW#6 done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+const name = prompt('Enter your name')
+const age = prompt('Enter your age')
+const likesProgramming = confirm('Do you like programming?');
+
+console.log(name)
+console.log(age)
+console.log(likesProgramming)
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof likesProgramming);
+
+let greeting = `Hi, ${name}`
+alert(greeting)
+
+let numericAge = Number(age);
+let nextAge = numericAge + 1
+console.log(nextAge)
+
+let isAdult = numericAge >= 18
+console.log(isAdult)
