e08405f855130de83a56618d60948b7f40bc6979
Lerois <lerois.ll@gmail.com>
2024-08-06 23:06:06 +0300

    HW#8 done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const num1 = prompt(`Please enter a 1st number`)
+const num2 = prompt(`Please enter a 2nd number`)
+const num3 = prompt(`Please enter a 3rd number`)
+
+if (num1 === null || isNaN(Number(num1)) || num2 === null || isNaN(Number(num2)) || num3 === null || isNaN(Number(num3))) {
+    alert('You entered not a valid number')
+}
+else  {
+    alert((Number(num1) + Number(num2) + Number(num3)) / 3)
+}
+
