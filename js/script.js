814739a9f4ea89ba8269bb5c27251d7978373ebd
Lerois <lerois.ll@gmail.com>
2024-08-06 22:28:00 +0300

    HW#7 done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,12 @@
 'use strict';
 
 
-const name = prompt('Enter your name')
-const age = prompt('Enter your age')
-const likesProgramming = confirm('Do you like programming?');
+const hoursInput = prompt('Please, input hours quantity to convert to seconds')
 
-console.log(name)
-console.log(age)
-console.log(likesProgramming)
-
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof likesProgramming);
-
-const greeting = `Hi, ${name}`
-alert(greeting)
-
-const numericAge = Number(age);
-const nextAge = numericAge + 1
-console.log(nextAge)
-
-const isAdult = numericAge >= 18
-console.log(isAdult)
+if (hoursInput === null || hoursInput === '' || isNaN(Number(hoursInput))) {
+    alert('It is not a joke. Please, enter a valid hours number');
+}
+else {
+    const timeInSeconds = Number(hoursInput) * 60 * 60;
+    alert(`${hoursInput} hours = ${timeInSeconds} seconds`);
+}
