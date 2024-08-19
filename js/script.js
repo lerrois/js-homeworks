af7f1a9778cd4eac00c10938e70723323c537e5e
Lerois <lerois.ll@gmail.com>
2024-08-19 21:31:52 +0300

    hw-14 done

--- js/script.js
+++ js/script.js
@@ -4,14 +4,12 @@ let numbers = ''
 for (let i = 40; i <= 60; i++) {
     numbers += i/2 + ' '
 }
-
 console.log(numbers);
 
 
 for (let i = 1; i <= 10; i++) {
     console.log(27 * i * 10 + ' ');
 }
-
 
 const n = prompt ('Введіть ціле число');
 
@@ -26,6 +24,18 @@ let simple = 'Simple'
 for (let i = 2; i <= n-1; i++) {
     if (n % i === 0) {
         simple = 'Not Simple'
+        break;
+    }
+}
+console.log(simple)
+
+const n = prompt ('Введіть ціле число');
+let multiply = 1;
+let simple = 'Multiply 3'
+while (multiply !== n) {
+    multiply = multiply * 3;
+    if (multiply > n) {
+        simple = 'Not Multiply 3';
         break;
     }
 }
