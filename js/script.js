e48583ac18091ee60d93029fee7cddbe77ec2bf1
Lerois <lerois.ll@gmail.com>
2024-08-27 19:23:18 +0300

    hw-15 done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,50 @@
 'use strict';
 
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-const key = generateKey(16, characters);
-console.log(key);
+const result = padString('aaa', 5, 'e', true)
+console.log(result)
 
-function generateKey(strlen, characters) {
-    let generated = '';
-    for (let i = 0; i < strlen; i++) {
-        generated += characters[Math.floor(Math.random() * strlen)];
+function padString(str, num, charVal, boolVal = false) {
+    const isValid = validate(str, num, charVal, boolVal)
+    if (typeof isValid === 'string') {
+        return isValid;
     }
-    return generated;
+
+    let strlen = str.length;
+    if (strlen > num) {
+        return str.substring(0, num)
+    }
+    const lengthStrAdd = num - strlen
+    let resultStr = '';
+
+    for (let i = 0; i < lengthStrAdd; i++) {
+        resultStr += charVal
+    }
+
+    if (boolVal) {
+        return resultStr + str;
+    }
+    return str + resultStr
+}
+
+function validate(str, num, charVal, boolVal) {
+    if (!Number.isInteger(num)) {
+        return 'Number of characters must be correct';
+    }
+    if (!(typeof str === 'string')) {
+        return 'String must be correct';
+    }
+    if ((typeof boolVal !== 'boolean')) {
+        return 'Position must be provided with boolean value.';
+    }
+    if ((typeof charVal !== 'undefined') && !(typeof charVal === 'string')) {
+        return 'Char must be provided. Or not be provided at all';
+    }
+    if (charVal.length !== 1) {
+        return 'Char must be provided with length 1.';
+    }
+    if (str.length > num && (typeof charVal === 'undefined')) {
+        return 'Provide Char for this string length';
+    }
+    return null;
 }
