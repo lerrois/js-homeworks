6c94ef3cd7e77191bf63425938c92a8e6389b453
Lerois <lerois.ll@gmail.com>
2024-08-27 19:17:15 +0300

    hw-15 done

--- Readme.md
+++ Readme.md
@@ -1,2 +1,10 @@
 
-opys
+Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
+
+Наприклад:
+
+Copy code
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const key = generateKey(16, characters);
+console.log(key);
+
+function generateKey(strlen, characters) {
+    let generated = '';
+    for (let i = 0; i < strlen; i++) {
+        generated += characters[Math.floor(Math.random() * strlen)];
+    }
+    return generated;
+}
