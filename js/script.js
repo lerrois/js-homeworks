f8e0b2255f4fe236110e3e23ae234a93ee7c8d0c
Lerois <lerois.ll@gmail.com>
2024-08-28 12:02:50 +0300

    hw-13 fixed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 'use strict';
-
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-const key = generateKey(16, characters);
-console.log(key); // eg599gb60q926j8i
-
-function generateKey(strlen, characters) {
-    if (!Number.isInteger(strlen) && !(typeof characters === 'string')) {
-        return 'Provided values are not correct.'
-    }
-    let generated = '';
-    for (let i = 0; i < strlen; i++) {
-        generated += characters[Math.floor(Math.random() * strlen)];
-    }
-    return generated;
+function generateKey(strlen, charactersInput) {
+  if (!Number.isInteger(strlen) && !(typeof charactersInput === 'string')) {
+    return 'Provided values are not correct.';
+  }
+  let generated = '';
+  for (let i = 0; i < strlen; i += 1) {
+    generated += charactersInput[Math.floor(Math.random() * strlen)];
+  }
+  return generated;
 }
+const key = generateKey(16, characters);
+console.log(key);
