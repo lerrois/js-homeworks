38df82d51c74230737c592c59440f8ee6d5a040b
Lerois <lerois.ll@gmail.com>
2024-08-30 21:35:56 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Readme.md
    #	js/script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function arithmeticalMean(array) {
+  let iterator = 0;
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i + 1) {
+    if (typeof array[i] === 'number') {
+      sum += array[i];
+      iterator += 1;
+    }
+  }
+
+  return sum / iterator;
+}
+
+const arr = ['apple', 3, 'orange', 5, 8, 'cherry'];
+
+const amean = arithmeticalMean(arr);
+console.log(`серендє арифметичне =${amean}`);
