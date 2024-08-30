27b6f598049e00d4ceb12b61076957f0230bef7f
Lerois <lerois.ll@gmail.com>
2024-08-30 21:53:13 +0300

    hw-17 done

--- js/script.js
+++ js/script.js
@@ -5,5 +5,4 @@ const array = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
 const removeElement = function (arr, item) {
   return arr.filter((word) => word !== item);
 };
-
 console.log(removeElement(array, 'elite'));
