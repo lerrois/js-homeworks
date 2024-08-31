a8f6778df9d13219623488f1f2f4a426e6f4ee3f
Lerois <lerois.ll@gmail.com>
2024-08-31 20:18:27 +0300

    hw-18 chek-in

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 function arithmeticalMean(array) {
   let iterator = 0;
   let sum = 0;
-
-  for (let i = 0; i < array.length; i + 1) {
+  console.log(array.length);
+  for (let i = 0; i < array.length; i++) {
     if (typeof array[i] === 'number') {
       sum += array[i];
       iterator += 1;
@@ -18,3 +18,59 @@ const arr = ['apple', 3, 'orange', 5, 8, 'cherry'];
 
 const amean = arithmeticalMean(arr);
 console.log(`серендє арифметичне =${amean}`);
+
+console.log('-----------------------------------------');
+
+const x = Number(prompt('введіть перше число'));
+const y = Number(prompt('введіть друге число'));
+const znak = prompt('введіть оператор');
+
+if (typeof x !== 'number' || typeof y !== 'number') {
+  alert('обидва числа мають бути числами');
+} else {
+  const doMath = function (xx, zznak, yy) {
+    if (zznak === '+') {
+      return xx + yy;
+    }
+    if (zznak === '-') {
+      return xx - yy;
+    }
+    if (zznak === '*') {
+      return xx * yy;
+    }
+    if (zznak === '/') {
+      return xx / yy;
+    }
+    if (zznak === '%') {
+      return xx % yy;
+    }
+    if (zznak === '^') {
+      return xx ** yy;
+    }
+    return 'error';
+  };
+  alert(doMath(x, znak, y));
+}
+console.log('-----------------------------------------');
+
+const first = prompt('введіть кількісь блоків данних');
+
+const array = [];
+let internalLength = 0;
+let internal = [];
+
+const inputData = function () {
+  for (let i = 0; i < first; i++) {
+    internalLength = prompt('введіть кількість айтемів у блоці');
+    internal = [];
+    for (let j = 0; j < internalLength; j++) {
+      internal[j] = prompt(`enter internal data for :${j}`);
+    }
+    array[i] = internal;
+  }
+  return array;
+};
+alert(inputData().join('\n'));
+console.log(array);
+
+console.log('-----------------------------------------');
