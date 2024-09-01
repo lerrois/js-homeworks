991725261927663ee6fbd6dd6dde1a9a228d865f
Lerois <lerois.ll@gmail.com>
2024-09-01 18:25:21 +0300

    hw-19 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const a = prompt('введіть число');
+const b = prompt('введіть ступінь');
+alert(a ** b);
+
+const exponentiation = function (num, exp) {
+  const result = num ** exp;
+  alert(result);
+};
+
+const mainFunction = function (callback) {
+  const numer = prompt('input number');
+  const expon = prompt('input exponentiaton');
+  return callback(numer, expon);
+};
+mainFunction(exponentiation);
