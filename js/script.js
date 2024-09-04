282b2278f60ada177fd1525cac2924d50ff93761
Lerois <lerois.ll@gmail.com>
2024-09-04 18:32:27 +0300

    hw-20 start

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const arrayLength = Number(prompt('введіть довжину масиву'));
+const array = [];
+const inputData = (arrLeng) => {
+  for (let i = 0; i < arrLeng; i++) {
+    array[i] = prompt('введіть число');
+  }
+  return array;
+};
+const arrNew = inputData(arrayLength);
+alert(arrNew);
+
+alert(arrNew.sort((a, b) => a - b));
+
+alert(arrNew.splice(2, 3));
