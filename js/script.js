fc0339b46066272c2b51a7fcf9ae276aab6314c7
Lerois <lerois.ll@gmail.com>
2024-09-18 15:47:57 +0200

    hw-27 start

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 'use strict';
 
+
+
+this.x = 9;
+const module = {
+  x: 81,
+  self: this,
+  getX() {
+    return this.self.x;
+  },
+};
+
+module.getX(); // 81
+
+const { getX } = module;
+getX(); // 9, поскольку в этом случае this ссылается на глобальный объект
+
+// создаём новую функцию с this, привязанным к module
+const boundGetX = getX();
+boundGetX(); // 81
+
+
+const modulefunc = (context, callback) => {
+    this = context
+  const newfunc = () => {}
+}
