b22c0cbdd22dcd1b028212342db7cb7148eb7840
Lerois <lerois.ll@gmail.com>
2024-09-19 17:40:54 +0200

    hw-28 start

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const factorialFunction = (n) => {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorialFunction(n - 1);
+};
+
+const result = factorialFunction(3);
+
+
 const func = () => {
 
-}
+};
