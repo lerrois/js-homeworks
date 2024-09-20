8a215fefbede4ea2c2f8e0ac31b7119c6c60a9ff
Lerois <lerois.ll@gmail.com>
2024-09-20 15:22:22 +0200

    hw-27 done

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 'use strict';
 
 const myBind = (func, context, ...args) => (...newArgs) => {
-  // Создаем массив аргументов, объединяя старые и новые
   const allArgs = [...args, ...newArgs];
-  // Вызываем функцию с указанным контекстом
   return func(...allArgs);
 };
 
-// Пример использования
-const greet = function(greeting, punctuation) {
-  return `${greeting}, ${this.name}${punctuation}`;
+// Пример
+const greet = function (greeting) {
+  return `${greeting}, ${this.name}`;
 };
 
 const person = { name: 'Olga' };
