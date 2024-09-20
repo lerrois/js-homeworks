f3d34dc55174d1c549419a6d47d63f24080706d5
Lerois <lerois.ll@gmail.com>
2024-09-20 19:53:25 +0200

    hw-29

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const complexArray = [
+  [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+  [5, [6, [7, [8, 9]]]],
+  [[10, 11], 12, 13],
+];
+
+const flatArr = (arr, result = []) => {
+  for (const item of arr) {
+    if (Array.isArray(item)) flatArr(item, result);
+    else result.push(item);
+  }
+  return result;
+};
+console.log(flatArr(complexArray));
