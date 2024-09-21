e58753eddc1c4083f9d520bb524438d491fd324c
Lerois <lerois.ll@gmail.com>
2024-09-21 21:04:48 +0200

    hw-30 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const func = () => {
+  let previousInput = 0;
+  let summa = 0;
+  const sum = (a) => {
+    summa = previousInput + a;
+    previousInput = a;
+    return summa;
+  };
+  return sum;
+};
+
+const sum = func();
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
