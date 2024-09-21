093432108272e50959d0060d27f8a7abc40abb85
Lerois <lerois.ll@gmail.com>
2024-09-21 20:46:28 +0200

    hw-31 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    alert(this.step);
+  },
+};
+
+ladder.up().up().down().showStep();
