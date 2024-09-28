66b140aed65857ca87abae47b5a591137beb8ce0
Lerois <lerois.ll@gmail.com>
2024-09-28 11:57:31 +0200

    hw-33 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const IMG_FOLDER = './images';
+const IMG_EXT = '.jpg';
+
+const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
+  if (!parentEl || !elementToAppend) return;
+  parentEl.append(elementToAppend);
+};
+
+const getRandomNumber = () => {
+  const num = Math.floor(Math.random() * 10);
+  if (num === 0) return 1;
+  return num;
+};
+
+const generateImg = (imgName) => {
+  const img = document.createElement('img');
+  img.src = `${IMG_FOLDER}/${imgName + IMG_EXT}`;
+  img.alt = `${IMG_FOLDER}/${imgName + IMG_EXT}`;
+  img.title = `${IMG_FOLDER}/${imgName + IMG_EXT}`;
+};
+
+const imgToAppend = generateImg(getRandomNumber());
+
+appendHTMLElement(document.body, imgToAppend);
