e47125726f91947353b3c8a02aae908f246fd217
Lerois <lerois.ll@gmail.com>
2024-09-30 21:38:02 +0200

    hw-33 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const appendHTMLElement = (parentElement = null, elementToAppend = null) => {
+  if (!parentElement || !elementToAppend) return;
+  parentElement.append(elementToAppend);
+};
+
+const generateTable = () => {
+  const tableSize = 10;
+  const table = document.createElement('table');
+  table.className = 'table';
+  const tbody = document.createElement('tbody');
+  table.append(tbody);
+  for (let i = 0; i <= tableSize; i++) {
+    const tr = document.createElement('tr');
+    for (let j = 0; j <= tableSize; j++) {
+      const td = document.createElement('td');
+      td.innerHTML = `${j}`;
+      tr.append(td);
+    }
+    tbody.append(tr);
+  }
+  return table;
+};
+
+appendHTMLElement(document.body, generateTable());
