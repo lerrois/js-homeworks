8dd8b6b4113e5f4a2c9db30b238bf984e504a300
Lerois <lerois.ll@gmail.com>
2024-10-03 15:24:56 +0200

    hw-34 fixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 'use strict';
 
-// Згенерувати div через js
-// Вставити його не сторінку
-// накинути івенти
-// написати функціями щоб можна було використовувати
-// протестувати на 3х таких дівах. (Уважно дивись на івент лісенери )
-
 const firstDiv = document.createElement('div');
-firstDiv.innerHTML = ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
+firstDiv.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
 
 const secondDiv = document.createElement('div');
 secondDiv.innerHTML = 'Lorem ipsum dolorLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
@@ -21,19 +15,25 @@ const appendElement = (elementToAppend) => {
 
 appendElement(firstDiv);
 
-const secondDivAdded = () => {
+const secondDivAppend = () => {
   appendElement(secondDiv);
 };
-
-const thirdDivAdded = () => {
+const thirdDivAppend = () => {
   appendElement(thirdDiv);
 };
 
-document.addEventListener('DOMContentLoaded', secondDivAdded);
-document.addEventListener('DOMContentLoaded', thirdDivAdded);
+const secondDivAdded = () => {
+  document.addEventListener('DOMContentLoaded', secondDivAppend);
+};
+const thirdDivAdded = () => {
+  document.addEventListener('DOMContentLoaded', thirdDivAppend);
+};
+secondDivAdded();
+thirdDivAdded();
 
 const div = document.createElement('div');
 div.innerHTML = 'Hello World!';
+div.classList.add('short');
 
 const divAppers = () => {
   document.body.append(div);
