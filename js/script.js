6790c60227efc8787b2111d1a86b82afdaf7ddda
Lerois <lerois.ll@gmail.com>
2024-10-23 21:53:54 +0200

    hw-41 done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
 'use strict';
 
-let product = {
-  name: "Joggers",
-  price: "100",
-  quantity: "100",
+const product = {
+  name: 'Joggers',
+  price: '100',
+  quantity: '100',
+  get totalValue() {
+    return `${this.price * this.quantity}`;
+  },
+  get getName() {
+    return this.name;
+  },
+  set setName(name) {
+    if (typeof name !== 'string' || name === '') {
+      throw new Error('Name must be a not empty string');
+    }
+    this.name = name;
+  },
+  get getPrice() {
+    return this.price;
+  },
+  set setPrice(value) {
+    const price = parseInt(value, 10);
+    if (Number.isNaN(price) || price < 0) {
+      throw new Error('Price must be more then 0');
+    }
+    this.price = price;
+  },
 };
 
 Object.defineProperty(product, 'name', {
-  value: "Joggers",
+  value: 'Joggers',
   writable: true,
 });
 
-Object.defineProperty(product, 'name', {
-  value: "Joggers",
+Object.defineProperty(product, 'price', {
   writable: true,
+  value: 100,
 });
 
-
-
-//Використовуючи Object.defineProperty, додайте властивість name, яка:
-//
-// Читається та записується.
-// Якщо ім'я не є рядком або пусте, не дозволяє встановити значення та виводить помилку в консоль.
-// Додайте властивість price, яка:
-//
-// Читається та записується.
-// Якщо ціна не є числом більше 0, не дозволяє встановити значення та виводить помилку.
-// Додайте властивість totalValue, яка обчислюється як price * quantity та є доступною лише для читання (геттер).
+product.setPrice = 55;
+console.log(product);
+console.log(product.totalValue);
