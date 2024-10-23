26b6eed4b0d9f35533965aa2e7e7855a95b4f32a
Lerois <lerois.ll@gmail.com>
2024-10-23 16:22:27 +0200

    hw-40 prepared

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 'use strict';
 
+const user = {
+  name: 'Lera',
+  age: '30',
+
+  get theName() {
+    return `${this.name}`;
+  },
+  set theName(value) {
+    this.name = value;
+  },
+//Сетер для age має перевіряти, щоб вік був числом від 0 до 120.
+// Якщо значення не підходить, виведіть помилку в консоль і не змінюйте значення.
+
+  get theAge() {
+    return `${this.age}`;
+  },
+  set theAge(value) {
+      if
+    this.age = value;
+  },
+};
+
+user.theName = 'Matilda';
+
+user.theAge = '26';
