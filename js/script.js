72f77d920a977e889864b787a28c4298a94ed36e
Lerois <lerois.ll@gmail.com>
2024-10-23 16:48:18 +0200

    hw-41 prepared

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 'use strict';
 
+let product = {
+  name: "Joggers",
+  price: "100",
+  quantity: "100",
+};
+
+Object.defineProperty(product, 'name', {
+  value: "Joggers",
+  writable: true,
+});
+
+Object.defineProperty(product, 'name', {
+  value: "Joggers",
+  writable: true,
+});
+
+
+
+//Використовуючи Object.defineProperty, додайте властивість name, яка:
+//
+// Читається та записується.
+// Якщо ім'я не є рядком або пусте, не дозволяє встановити значення та виводить помилку в консоль.
+// Додайте властивість price, яка:
+//
+// Читається та записується.
+// Якщо ціна не є числом більше 0, не дозволяє встановити значення та виводить помилку.
+// Додайте властивість totalValue, яка обчислюється як price * quantity та є доступною лише для читання (геттер).
