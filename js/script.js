82224d5eee80c53e23c0aca7a5a0ada87f48da0a
Lerois <lerois.ll@gmail.com>
2024-12-09 14:41:47 +0100

    hw-41 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const postnumberIn = () => parseInt(prompt('Enter your post number'));
+
+const fetchFunc = (value) => {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${value}`)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+async function getpost() {
+  const numberinput = await postnumberIn();
+  if (!isNaN(numberinput) && numberinput > 0 && numberinput <= 100) {
+    await fetchFunc(numberinput);
+  } else throw Error('Unable to get post number');
+}
+getpost();
