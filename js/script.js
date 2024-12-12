0da517b4a878d9e87fc0ea876bb7011c452e3c1a
Lerois <lerois.ll@gmail.com>
2024-12-12 18:51:46 +0100

    hw-41 done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// const answer = prompt('Insert your city');
-// const answer = 'KYIV111';
-const answer = 'KYIV';
+const answer = prompt('Insert your city');
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${answer}&units=metric&appid=5d066958a60d315387d9492393935c19`)
   .then((response) => response.json())
@@ -10,15 +8,19 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${answer}&units=metric&
     if (data.cod !== 200) {
       throw Error('Could not find any weather');
     }
-    console.log(`temp = ${data.main.temp}`);
-    console.log(`pressure = ${data.main.pressure}`);
-    console.log(`description = ${data.main.description}`);
-    console.log(`humidity = ${data.main.humidity}`);
-    console.log(`speed = ${data.wind.speed}`);
-    console.log(`deg = ${data.wind.deg}`);
+    const elem = document.getElementById('wrapper');
+    const span = document.createElement('span');
     const { icon } = data.weather[0];
-    console.log(`http://openweathermap.org/img/w/${icon}.png`);
+    const logo = `http://openweathermap.org/img/w/${icon}.png`;
+
+    span.innerHTML = `temp = ${data.main.temp} <br> 
+                   pressure = ${data.main.pressure} <br> 
+                   description = ${data.main.description} <br>
+                   humidity = ${data.main.humidity} <br>
+                   speed = ${data.wind.speed} <br>
+                   deg = ${data.wind.deg} <br>
+                   <img src="${logo}">            
+    `;
+    elem.append(span);
   })
   .catch((err) => alert(err));
-
-
